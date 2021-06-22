341329f673ac35bfb546f97c568ce848a48927e8
Sangmun Jeong <akios112@gmail.com>
2021-06-22 19:27:09 +0900

    TestController추가

---
+++ src/test/kotlin/oioi/back/oiback/OioiApplicationTests.kt
@@ -0,0 +1,13 @@
+package oioi.back.oiback
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OioiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
