4b9ede10b94ea74ac4964e507e8cd46fd96be192
SSRyu <jjkztok3@gmail.com>
2021-07-10 17:07:01 +0900

    Add PostResponse

---
+++ src/main/kotlin/oioi/back/oiback/post/dto/PostResponse.kt
@@ -0,0 +1,27 @@
+package oioi.back.oiback.post.dto
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import java.time.LocalDateTime
+
+data class PostResponse(
+    @JsonProperty("title")
+    val title: String,
+    @JsonProperty("author")
+    val author: String,
+    @JsonProperty("content")
+    val content: String,
+
+    @JsonProperty("view")
+    val view: Long = 0L,
+    @JsonProperty("like")
+    val like: Long = 0L,
+    @JsonProperty("createdAt")
+    val createdAt: LocalDateTime,
+    @JsonProperty("updatedAt")
+    val updatedAt: LocalDateTime,
+
+//    @JsonProperty("replies")
+//    val replies: List<Reply> = mutableListOf(),
+//    @JsonProperty("tags")
+//    val tags: List<Tag> =  = mutableListOf(),
+)
