34db17c98e475fdebe88b488438779824666bbe9
SSRyu <jjkztok3@gmail.com>
2021-11-21 16:14:27 +0900

    Add detekt config

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
     id("org.flywaydb.flyway") version "8.0.2"
+    id("io.gitlab.arturbosch.detekt") version "1.18.0"
     kotlin("jvm") version "1.5.10"
     kotlin("plugin.spring") version "1.5.10"
 }
@@ -53,4 +54,18 @@ flyway {
     url = "jdbc:mysql://localhost:3306/oi?&allowPublicKeyRetrieval=true&useSSL=false"
     user = "username"
     password = "password"
+}
+
+detekt {
+    buildUponDefaultConfig = true // preconfigure defaults
+    allRules = false // activate all available (even unstable) rules.
+    config = files("$projectDir/config/detekt.yml") // point to your custom config defining rules to run, overwriting default behavior
+    baseline = file("$projectDir/config/baseline.xml") // a way of suppressing issues before introducing detekt
+
+    reports {
+    html.enabled = true // observe findings in your browser with structure and code snippets
+    xml.enabled = true // checkstyle like format mainly for integrations like Jenkins
+    txt.enabled = true // similar to the console output, contains issue signature to manually edit baseline files
+    sarif.enabled = true // standardized SARIF format (https://sarifweb.azurewebsites.net/) to support integrations with Github Code Scanning
+    }
 }
